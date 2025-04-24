3151da899295de8991c3cb8ab9312bf84d5bf61f
RodrigoPeres49 <130695825+RodrigoPeres49@users.noreply.github.com>
2025-04-23 22:50:51 -0300

    Atualizar os arquivos

---
+++ src/main/java/com/example/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,52 @@
+package com.example.CadastroDeNinjas;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// Transformar classe em entidade do banco de dados
+@Entity
+@Table(name = "tb_cadastro")
+
+public class NinjaModel {
+
+    @Id
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
+
+
